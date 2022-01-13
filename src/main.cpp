fe30d0a047d3f426043292dc5e4ee30aadfcb0f8
fxb <931935931@qq.com>
2022-01-13 21:50:52 +0800

    测试

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * @Author: fengxb
  * @Date: 2022-01-13 21:04:49
  * @LastEditor: fengxb
- * @LastEditTime: 2022-01-13 21:29:37
+ * @LastEditTime: 2022-01-13 21:32:34
  */
 #include <iostream>
 #include <vector>
@@ -11,5 +11,4 @@
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello world!" << std::endl;
-
 }
