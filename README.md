bdcd013e91135a806303f99206ae7b4f84816945
fxb <931935931@qq.com>
2022-01-13 20:59:33 +0800

    测试

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# LeetCode
+<!--
+ * @Description: 题纲
+ * @Author: fengxb
+ * @Date: 2022-01-12 15:00:39
+ * @LastEditor: fengxb
+ * @LastEditTime: 2022-01-13 19:51:44
+-->
+##LeetCode
 力扣刷题
-时间：2022-1-13
 
-第一题：
-1.两数之和
+#### 第一部分：数组
+1.704二分查找
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+/*
+ * @Description: 
+ * @Author: fengxb
+ * @Date: 2022-01-13 19:47:04
+ * @LastEditor: fengxb
+ * @LastEditTime: 2022-01-13 20:59:08
+ */
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	
+
+	return 0;
 }
