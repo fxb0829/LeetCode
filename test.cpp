1365736026c787060336d71ed211379fc9cf04bd
fxb <931935931@qq.com>
2022-01-14 20:32:18 +0800

    修改l了配置文件

---
+++ test.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Description: 
+ * @Author: fengxb
+ * @Date: 2022-01-14 17:10:47
+ * @LastEditor: fengxb
+ * @LastEditTime: 2022-01-14 20:06:48
+ */
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+
+    //vector数组
+    vector<int> arr = {-1,1,2,2,3,5};
+    int firstLoc = upper_bound(arr.begin(),arr.end(),2)-arr.begin();
+    cout << firstLoc << endl;
+    return 0;
+}
