26e17905579c6b1afbb9f2de6c8d61233c93300c
fxb <931935931@qq.com>
2022-01-18 23:24:26 +0800

    2022-01-18

--- 数组/test.cpp
+++ 数组/test.cpp
@@ -15,8 +15,7 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size()-1;
-        while (left <= right)
-        {
+        while (left <= right){
             int mid = (left+right)>>1;
             if(nums[mid] == target)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
             {
